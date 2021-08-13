d12bc092a660ef1fc8bd541287624e74c8a74246
Kazunori Kimura <kazunori.kimura.js@gmail.com>
2021-08-13 11:35:46 +0900

    [mod] update docs

--- types/components/hook.d.ts
+++ types/components/hook.d.ts
@@ -2,4 +2,4 @@ import { TableHookParameters, TableHookReturns } from './types';
 /**
  * Table の props を生成するカスタム Hooks
  */
-export declare const useTable: <T>({ items, columns, getRowKey, onChange, page, rowsPerPage, rowsPerPageOptions, options, messages, readOnly, }: TableHookParameters<T>) => TableHookReturns<T>;
+export declare const useTable: <T>({ items, columns, getRowKey, onChange, page, rowsPerPage, rowsPerPageOptions, options, messages, readOnly, disableUndo, }: TableHookParameters<T>) => TableHookReturns<T>;
